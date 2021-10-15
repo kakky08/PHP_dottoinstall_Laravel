1f498e5e940b721c1ea49aff74cc6065a8aead81
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-10-15 23:57:03 +0900

    Blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,26 @@
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="utf-8">
     <title>My BBS</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>My BBS</h1>
-    <ul>
-        <li>Title</li>
-        <li>Title</li>
-        <li>Title</li>
-    </ul>
+    <div class="container">
+        <h1>My BBS</h1>
+        <ul>
+            {{-- @foreach ($posts as $post)
+                <li>{{ $post }}</li>
+            @endforeach --}}
+
+            @forelse ($posts as $post)
+                <li>{{ $post }}</li>
+            @empty
+                <li>No posts yet!</li>
+            @endforelse
+        </ul>
+    </div>
 </body>
 </html>
